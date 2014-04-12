8c626fd52ff495df90dfbd5c463d99e0d74a0dbc
Venura Kahawala <vkahawala@gmail.com>
2014-04-12 15:15:37 +0800

    adding the backend core project

---
+++ customer.mgt.core/src/main/java/customer/mgt/App.java
@@ -0,0 +1,13 @@
+package customer.mgt;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
